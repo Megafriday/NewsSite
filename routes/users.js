dc10c5c68b24cdc928294c7aadcf75448ea28a91
megafriday <megafriday@yahoo.co.jp>
2020-07-18 22:39:25 +0900

    usersのロジック追加

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
+const gnews = require('gnews');
 var express = require('express');
 var router = express.Router();
 
+const config = {
+	country: 'jp',
+	language: 'ja',
+	n: 5
+};
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+
+	async function main() {
+		const healths = await gnews.topic('HEALTH', config);
+
+		res.render('users', { healths });
+	}
+	main();
 });
 
 module.exports = router;
